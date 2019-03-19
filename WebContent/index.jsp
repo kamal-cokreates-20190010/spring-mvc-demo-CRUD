ba38dede93ee44f9812eb142ff8777ffc749939e
Kamal Parvez <kamal@cokreates.com>
2019-03-20 02:06:37 +0600

    spring-mvc-demo-assignment-2_2 completed

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -3,6 +3,11 @@
 <head>
 <body>
 <h2>Welcome to my Home Page</h2>
+<a href="customer/showFormForAdd">Customer Add Page</a>
+<br><br>
+<a href="customer/list">Customer List Page</a>
+
+<br><br>
 </body>
 </head>
 </html>
---
+++ src/ckl/kamal/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package ckl.kamal.springdemo.service;
+
+import java.util.List;
+
+import ckl.kamal.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+
+	
+}
